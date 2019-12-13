06b8037d6c07f97c74b82e22f8771887fe0b6a51
avikola <avishkar1995@gmail.com>
2019-12-12 23:43:23 -0800

    recheck

--- jello.cpp
+++ jello.cpp
@@ -1,39 +1,31 @@
-/*
-
-  USC/Viterbi/Computer Science
-  "Jello Cube" Assignment 1
-
-  Your name:
-  Avishkar Kolahalu - 6138 4283 68
-
-*/
+//	Avishkar Kolahalu - 6138 4283 68
 
 #include "jello.h"
 #include "showCube.h"
 #include "input.h"
 #include "physics.h"
 
-// camera parameters
+// Camera parameters
 double Theta = pi / 6;
 double Phi = pi / 6;
 double R = 6;
 
-// mouse control
+// Mouse control
 int g_iMenuId;
 int g_vMousePos[2];
 int g_iLeftMouseButton,g_iMiddleMouseButton,g_iRightMouseButton;
 
-// number of images saved to disk so far
+// Number of images saved to disk so far
 int sprite=0;
 
-// these variables control what is displayed on screen
+// These variables control what is displayed on screen
 int shear=0, bend=0, structural=1, pause=0, viewingMode=0, saveScreenToFile=0;
 
 struct world jello;
 
 int windowWidth, windowHeight;
 
-// Inclined Plane Variables.
+// Inclined Plane Variables
 struct point Normal, intersect_list[5];
 int total_intersect = 0;
 
@@ -145,7 +137,7 @@ void planeIntersect()
 
 void myinit()
 {
-	// If a plane exists, calculate intersection.
+	// If a plane exists, calculate intersection
 	if (jello.incPlanePresent == 1)
 		planeIntersect();
 
@@ -153,7 +145,7 @@ void myinit()
 	glLoadIdentity();
 	gluPerspective(90.0,1.0,0.01,1000.0);
 
-	// set background color to grey
+	// Set background color to grey
 	glClearColor(0.2, 0.2, 0.2, 0.0);
 
 	glCullFace(GL_BACK);
@@ -168,176 +160,173 @@ void myinit()
 
 void reshape(int w, int h) 
 {
-  // Prevent a divide by zero, when h is zero.
-  // You can't make a window of zero height.
-  if(h == 0)
-    h = 1;
+	// Prevent a divide by zero, when h is zero
+	// You can't make a window of zero height
+	if(h == 0)
+	h = 1;
 
-  glViewport(0, 0, w, h);
+	glViewport(0, 0, w, h);
 
-  // Reset the coordinate system before modifying
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  // Set the perspective
-  double aspectRatio = 1.0 * w / h;
-  gluPerspective(60.0f, aspectRatio, 0.01f, 1000.0f);
+	// Reset the coordinate system before modifying
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
 
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity(); 
+	// Set the perspective
+	double aspectRatio = 1.0 * w / h;
+	gluPerspective(60.0f, aspectRatio, 0.01f, 1000.0f);
 
-  windowWidth = w;
-  windowHeight = h;
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity(); 
 
-  glutPostRedisplay();
+	windowWidth = w;
+	windowHeight = h;
+
+	glutPostRedisplay();
 }
 
 void display()
 {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-  glMatrixMode(GL_MODELVIEW);
-  glLoadIdentity();
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 
-  // camera parameters are Phi, Theta, R
-  gluLookAt(R * cos(Phi) * cos (Theta), R * sin(Phi) * cos (Theta), R * sin (Theta),
-	        0.0,0.0,0.0, 0.0,0.0,1.0);
+	// Camera parameters are Phi, Theta, R
+	gluLookAt(R * cos(Phi) * cos (Theta), R * sin(Phi) * cos (Theta), R * sin (Theta),
+			0.0,0.0,0.0, 0.0,0.0,1.0);
 
-  /* Lighting */
-  /* You are encouraged to change lighting parameters or make improvements/modifications
-     to the lighting model . 
-     This way, you will personalize your assignment and your assignment will stick out. 
-  */
+	// Lighting:
 
-  // global ambient light
-  GLfloat aGa[] = { 0.0, 0.0, 0.0, 0.0 };
+	// Global ambient light
+	GLfloat aGa[] = { 0.0, 0.0, 0.0, 0.0 };
   
-  // light 's ambient, diffuse, specular
-  GLfloat lKa0[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat lKd0[] = { 1.0, 1.0, 1.0, 1.0 };
-  GLfloat lKs0[] = { 1.0, 1.0, 1.0, 1.0 };
+	// Light's ambient, diffuse, specular
+	GLfloat lKa0[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat lKd0[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat lKs0[] = { 1.0, 1.0, 1.0, 1.0 };
 
-  GLfloat lKa1[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat lKd1[] = { 1.0, 0.0, 0.0, 1.0 };
-  GLfloat lKs1[] = { 1.0, 0.0, 0.0, 1.0 };
+	GLfloat lKa1[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat lKd1[] = { 1.0, 0.0, 0.0, 1.0 };
+	GLfloat lKs1[] = { 1.0, 0.0, 0.0, 1.0 };
 
-  GLfloat lKa2[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat lKd2[] = { 1.0, 1.0, 0.0, 1.0 };
-  GLfloat lKs2[] = { 1.0, 1.0, 0.0, 1.0 };
+	GLfloat lKa2[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat lKd2[] = { 1.0, 1.0, 0.0, 1.0 };
+	GLfloat lKs2[] = { 1.0, 1.0, 0.0, 1.0 };
 
-  GLfloat lKa3[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat lKd3[] = { 0.0, 1.0, 1.0, 1.0 };
-  GLfloat lKs3[] = { 0.0, 1.0, 1.0, 1.0 };
+	GLfloat lKa3[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat lKd3[] = { 0.0, 1.0, 1.0, 1.0 };
+	GLfloat lKs3[] = { 0.0, 1.0, 1.0, 1.0 };
 
-  GLfloat lKa4[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat lKd4[] = { 0.0, 0.0, 1.0, 1.0 };
-  GLfloat lKs4[] = { 0.0, 0.0, 1.0, 1.0 };
+	GLfloat lKa4[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat lKd4[] = { 0.0, 0.0, 1.0, 1.0 };
+	GLfloat lKs4[] = { 0.0, 0.0, 1.0, 1.0 };
 
-  GLfloat lKa5[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat lKd5[] = { 1.0, 0.0, 1.0, 1.0 };
-  GLfloat lKs5[] = { 1.0, 0.0, 1.0, 1.0 };
+	GLfloat lKa5[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat lKd5[] = { 1.0, 0.0, 1.0, 1.0 };
+	GLfloat lKs5[] = { 1.0, 0.0, 1.0, 1.0 };
 
-  GLfloat lKa6[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat lKd6[] = { 1.0, 1.0, 1.0, 1.0 };
-  GLfloat lKs6[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat lKa6[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat lKd6[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat lKs6[] = { 1.0, 1.0, 1.0, 1.0 };
 
-  GLfloat lKa7[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat lKd7[] = { 0.0, 1.0, 1.0, 1.0 };
-  GLfloat lKs7[] = { 0.0, 1.0, 1.0, 1.0 };
+	GLfloat lKa7[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat lKd7[] = { 0.0, 1.0, 1.0, 1.0 };
+	GLfloat lKs7[] = { 0.0, 1.0, 1.0, 1.0 };
 
-  // light positions and directions
-  GLfloat lP0[] = { -1.999, -1.999, -1.999, 1.0 };
-  GLfloat lP1[] = { 1.999, -1.999, -1.999, 1.0 };
-  GLfloat lP2[] = { 1.999, 1.999, -1.999, 1.0 };
-  GLfloat lP3[] = { -1.999, 1.999, -1.999, 1.0 };
-  GLfloat lP4[] = { -1.999, -1.999, 1.999, 1.0 };
-  GLfloat lP5[] = { 1.999, -1.999, 1.999, 1.0 };
-  GLfloat lP6[] = { 1.999, 1.999, 1.999, 1.0 };
-  GLfloat lP7[] = { -1.999, 1.999, 1.999, 1.0 };
+	// Light positions and directions
+	GLfloat lP0[] = { -1.999, -1.999, -1.999, 1.0 };
+	GLfloat lP1[] = { 1.999, -1.999, -1.999, 1.0 };
+	GLfloat lP2[] = { 1.999, 1.999, -1.999, 1.0 };
+	GLfloat lP3[] = { -1.999, 1.999, -1.999, 1.0 };
+	GLfloat lP4[] = { -1.999, -1.999, 1.999, 1.0 };
+	GLfloat lP5[] = { 1.999, -1.999, 1.999, 1.0 };
+	GLfloat lP6[] = { 1.999, 1.999, 1.999, 1.0 };
+	GLfloat lP7[] = { -1.999, 1.999, 1.999, 1.0 };
   
-  // jelly material color
+	// Jelly material color
 
-  GLfloat mKa[] = { 0.0, 0.0, 0.0, 1.0 };
-  GLfloat mKd[] = { 0.3, 0.3, 0.3, 1.0 };
-  GLfloat mKs[] = { 1.0, 1.0, 1.0, 1.0 };
-  GLfloat mKe[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat mKa[] = { 0.0, 0.0, 0.0, 1.0 };
+	GLfloat mKd[] = { 0.3, 0.3, 0.3, 1.0 };
+	GLfloat mKs[] = { 1.0, 1.0, 1.0, 1.0 };
+	GLfloat mKe[] = { 0.0, 0.0, 0.0, 1.0 };
 
-  /* set up lighting */
-  glLightModelfv(GL_LIGHT_MODEL_AMBIENT, aGa);
-  glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
-  glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
+	// Set up lighting
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, aGa);
+	glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
+	glLightModelf(GL_LIGHT_MODEL_TWO_SIDE, GL_FALSE);
 
-  // set up cube color
-  glMaterialfv(GL_FRONT, GL_AMBIENT, mKa);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE, mKd);
-  glMaterialfv(GL_FRONT, GL_SPECULAR, mKs);
-  glMaterialfv(GL_FRONT, GL_EMISSION, mKe);
-  glMaterialf(GL_FRONT, GL_SHININESS, 120);
+	// Set up cube color
+	glMaterialfv(GL_FRONT, GL_AMBIENT, mKa);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, mKd);
+	glMaterialfv(GL_FRONT, GL_SPECULAR, mKs);
+	glMaterialfv(GL_FRONT, GL_EMISSION, mKe);
+	glMaterialf(GL_FRONT, GL_SHININESS, 120);
     
-  // macro to set up light i
-  #define LIGHTSETUP(i)\
-  glLightfv(GL_LIGHT##i, GL_POSITION, lP##i);\
-  glLightfv(GL_LIGHT##i, GL_AMBIENT, lKa##i);\
-  glLightfv(GL_LIGHT##i, GL_DIFFUSE, lKd##i);\
-  glLightfv(GL_LIGHT##i, GL_SPECULAR, lKs##i);\
-  glEnable(GL_LIGHT##i)
+	// Macro to set up light i
+	#define LIGHTSETUP(i)\
+	glLightfv(GL_LIGHT##i, GL_POSITION, lP##i);\
+	glLightfv(GL_LIGHT##i, GL_AMBIENT, lKa##i);\
+	glLightfv(GL_LIGHT##i, GL_DIFFUSE, lKd##i);\
+	glLightfv(GL_LIGHT##i, GL_SPECULAR, lKs##i);\
+	glEnable(GL_LIGHT##i)
   
-  LIGHTSETUP (0);
-  LIGHTSETUP (1);
-  LIGHTSETUP (2);
-  LIGHTSETUP (3);
-  LIGHTSETUP (4);
-  LIGHTSETUP (5);
-  LIGHTSETUP (6);
-  LIGHTSETUP (7);
+	LIGHTSETUP (0);
+	LIGHTSETUP (1);
+	LIGHTSETUP (2);
+	LIGHTSETUP (3);
+	LIGHTSETUP (4);
+	LIGHTSETUP (5);
+	LIGHTSETUP (6);
+	LIGHTSETUP (7);
 
-  // enable lighting
-  glEnable(GL_LIGHTING);    
-  glEnable(GL_DEPTH_TEST);
+	// Enable lighting
+	glEnable(GL_LIGHTING);    
+	glEnable(GL_DEPTH_TEST);
 
-  // show the cube
-  showCube(&jello);
+	// Show the cube
+	showCube(&jello);
 
-  glDisable(GL_LIGHTING);
+	glDisable(GL_LIGHTING);
 
-  // show the bounding box
-  showBoundingBox();
+	// Show the bounding box
+	showBoundingBox();
 
-  // If there exists an inclined plane:
+	// If there exists an inclined plane:
 
-  if (jello.incPlanePresent == 1)
-  {
-	  // Both sides, with different colours, of the plane:
+	if (jello.incPlanePresent == 1)
+	{
+		// Both sides, with different colours, of the plane:
 
-	  int i = 0;
+		int i = 0;
 
-	  glFrontFace(GL_CW);
-	  glColor4f(1.0, 0.65, 0.0, 0);
+		glFrontFace(GL_CW);
+		glColor4f(1.0, 0.65, 0.0, 0);
 
-	  glBegin(GL_POLYGON);
+		glBegin(GL_POLYGON);
 
-	  glVertex3f(intersect_list[i].x, intersect_list[i].y, intersect_list[i].z);
-	  glVertex3f(intersect_list[i + 3].x, intersect_list[i + 3].y, intersect_list[i + 3].z);
-	  glVertex3f(intersect_list[i + 1].x, intersect_list[i + 1].y, intersect_list[i + 1].z);
-	  glVertex3f(intersect_list[i + 2].x, intersect_list[i + 2].y, intersect_list[i + 2].z);
+		glVertex3f(intersect_list[i].x, intersect_list[i].y, intersect_list[i].z);
+		glVertex3f(intersect_list[i + 3].x, intersect_list[i + 3].y, intersect_list[i + 3].z);
+		glVertex3f(intersect_list[i + 1].x, intersect_list[i + 1].y, intersect_list[i + 1].z);
+		glVertex3f(intersect_list[i + 2].x, intersect_list[i + 2].y, intersect_list[i + 2].z);
 
-	  glEnd();
+		glEnd();
 
-	  glFrontFace(GL_CCW);
-	  glColor4f(0.4, 0.0, 0.0, 0);
+		glFrontFace(GL_CCW);
+		glColor4f(0.4, 0.0, 0.0, 0);
 
-	  glBegin(GL_POLYGON);
+		glBegin(GL_POLYGON);
 
-	  glVertex3f(intersect_list[i].x, intersect_list[i].y, intersect_list[i].z);
-	  glVertex3f(intersect_list[i + 3].x, intersect_list[i + 3].y, intersect_list[i + 3].z);
-	  glVertex3f(intersect_list[i + 1].x, intersect_list[i + 1].y, intersect_list[i + 1].z);
-	  glVertex3f(intersect_list[i + 2].x, intersect_list[i + 2].y, intersect_list[i + 2].z);
+		glVertex3f(intersect_list[i].x, intersect_list[i].y, intersect_list[i].z);
+		glVertex3f(intersect_list[i + 3].x, intersect_list[i + 3].y, intersect_list[i + 3].z);
+		glVertex3f(intersect_list[i + 1].x, intersect_list[i + 1].y, intersect_list[i + 1].z);
+		glVertex3f(intersect_list[i + 2].x, intersect_list[i + 2].y, intersect_list[i + 2].z);
 
-	  glEnd();
+		glEnd();
 
-	  // Show a black outline for the plane:
+		// Show a black outline for the plane:
 
-	  glColor4f(0, 0, 0, 1.0);
-	  glBegin(GL_LINES);
+		glColor4f(0, 0, 0, 1.0);
+		glBegin(GL_LINES);
 		glVertex3f(intersect_list[3].x, intersect_list[3].y, intersect_list[3].z);
 		glVertex3f(intersect_list[1].x, intersect_list[1].y, intersect_list[1].z);
 		glVertex3f(intersect_list[2].x, intersect_list[2].y, intersect_list[2].z);
@@ -346,110 +335,87 @@ void display()
 		glVertex3f(intersect_list[3].x, intersect_list[3].y, intersect_list[3].z);
 		glVertex3f(intersect_list[1].x, intersect_list[1].y, intersect_list[1].z);
 		glVertex3f(intersect_list[2].x, intersect_list[2].y, intersect_list[2].z);
-	  glEnd();
+		glEnd();
 
-  }
+	}
 
-  glutSwapBuffers();
+	glutSwapBuffers();
 }
 
 void doIdle()
 {
-  char s[20]="picxxxx.ppm";
-  int i, delayer = 0;
+	char s[20]="picxxxx.ppm";
+	int i, delayer = 0;
   
-  // save screen to file
-  s[3] = 48 + (sprite / 1000);
-  s[4] = 48 + (sprite % 1000) / 100;
-  s[5] = 48 + (sprite % 100 ) / 10;
-  s[6] = 48 + sprite % 10;
+	// save screen to file
+	s[3] = 48 + (sprite / 1000);
+	s[4] = 48 + (sprite % 1000) / 100;
+	s[5] = 48 + (sprite % 100 ) / 10;
+	s[6] = 48 + sprite % 10;
 
-  if (saveScreenToFile==1)
-  {
-	  if (delayer == 0)
-	  {
-		  saveScreenshot(windowWidth, windowHeight, s);
-		  sprite++;
-		  delayer = 1;
-	  }
-	  else 
-		  delayer = 0;
+	if (saveScreenToFile==1)
+	{
+		if (delayer == 0)
+		{
+			saveScreenshot(windowWidth, windowHeight, s);
+			sprite++;
+			delayer = 1;
+		}
+		else 
+			delayer = 0;
 
-    // saveScreenToFile=0; // save only once, change this if you want continuos image generation (i.e. animation)
-  }
+	// saveScreenToFile=0; save only once, change this if you want continuos image generation (i.e. animation)
+	}
 
-  if (sprite >= 300) // allow only 300 snapshots
-  {
-    exit(0);	
-  }
+	// Allow only 300 snapshots
+	if (sprite >= 300)
+		exit(0);
 
-  if (pause == 0)
-  {
-	  // Appropriate method chosen:
+	if (pause == 0)
+	{
+		// Appropriate method chosen:
 
-	  if (strcmp("Euler", jello.integrator) == 0)
-	  {
-		  Euler(&jello);
-	  }
+		if (strcmp("Euler", jello.integrator) == 0)
+			Euler(&jello);
 
-	  if (strcmp("RK4", jello.integrator) == 0)
-	  {
-		  RK4(&jello);
-	  }
+		if (strcmp("RK4", jello.integrator) == 0)
+			RK4(&jello);
+	}
 
-  }
-
-  glutPostRedisplay();
+	glutPostRedisplay();
 }
 
 int main (int argc, char ** argv)
 {
-  if (argc<2)
-  {  
-    printf ("Oops! You didn't say the jello world file!\n");
-    printf ("Usage: %s [worldfile]\n", argv[0]);
-    exit(0);
-  }
+	if (argc<2)
+	{  
+		printf ("Oops! You didn't say the jello world file!\n");
+		printf ("Usage: %s [worldfile]\n", argv[0]);
+		exit(0);
+	}
 
-  readWorld(argv[1],&jello);
+	readWorld(argv[1],&jello);
 
-  glutInit(&argc,argv);
+	glutInit(&argc,argv);
   
-  /* double buffered window, use depth testing, 640x480 */
-  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	// double buffered window, use depth testing, 640x480
+	glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   
-  windowWidth = 640;
-  windowHeight = 480;
-  glutInitWindowSize (windowWidth, windowHeight);
-  glutInitWindowPosition (0,0);
-  glutCreateWindow ("Jello Cube - Avishkar Kolahalu");
+	windowWidth = 640;
+	windowHeight = 480;
+	glutInitWindowSize (windowWidth, windowHeight);
+	glutInitWindowPosition (0,0);
 
-  /* tells glut to use a particular display function to redraw */
-  glutDisplayFunc(display);
+	glutCreateWindow ("Jello Cube - Avishkar Kolahalu");
+	glutDisplayFunc(display);
+	glutIdleFunc(doIdle);
+	glutMotionFunc(mouseMotionDrag);
+	glutReshapeFunc(reshape);
+	glutPassiveMotionFunc(mouseMotion);
+	glutMouseFunc(mouseButton);
+	glutKeyboardFunc(keyboardFunc);
+	myinit();
+	glutMainLoop();
 
-  /* replace with any animate code */
-  glutIdleFunc(doIdle);
-
-  /* callback for mouse drags */
-  glutMotionFunc(mouseMotionDrag);
-
-  /* callback for window size changes */
-  glutReshapeFunc(reshape);
-
-  /* callback for mouse movement */
-  glutPassiveMotionFunc(mouseMotion);
-
-  /* callback for mouse button changes */
-  glutMouseFunc(mouseButton);
-
-  /* register for keyboard events */
-  glutKeyboardFunc(keyboardFunc);
-
-  /* do initialization */
-  myinit();
-
-  /* forever sink in the black hole */
-  glutMainLoop();
-
-  return(0);
+	return(0);
 }
